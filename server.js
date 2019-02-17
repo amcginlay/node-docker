da9e861da1b786583873cff011face84297fd732
Alan McGinlay <amcginlay@pivotal.io>
2019-02-17 15:08:22 +0000

    Take greeting for env var

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const GREETING = process.env.GREETING || 'Hello'
+
 require('http').createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello from ' + require('os').hostname() + '\n');
+    res.end(GREETING + ' from ' + require('os').hostname() + '\n');
 }).listen(8080);
 console.log('Server running on 8080');

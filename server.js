1749f3f67b81264078ed544e9602841822458664
Alan McGinlay <amcginlay@pivotal.io>
2019-02-20 20:34:16 +0000

    allow port configuration

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const GREETING = process.env.GREETING || 'Hello'
+const PORT = process.env.PORT || 8080
 
 require('http').createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(GREETING + ' from ' + require('os').hostname() + '\n');
-}).listen(8080);
-console.log('Server running on 8080');
+}).listen(PORT);
+console.log('Server running on ' + PORT);
